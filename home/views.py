892e30ac151819c9647322f5533228d10d69ec77
Ian Leocádio <ian.leocadio@techne.com.br>
2018-05-11 11:07:05 -0300

    Aula CBV

--- home/views.py
+++ home/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
+from django.views.generic.base import TemplateView
 
 
+
+
+# TODO: Refatorar para usar threads assim que possivel
 def home(request):
     return render(request, 'home.html')
 
-
+# FIXME: Corrigir bug ....
 def my_logout(request):
     logout(request)
     return redirect('home')
+
+
+class HomePageView(TemplateView):
+    template_name = "home2.html"
